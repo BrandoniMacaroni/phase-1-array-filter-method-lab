0cb869ca7d92e688a8dbbe7bf0a6f8838054503a
Brandon <bcharris713@gmail.com>
2022-05-22 15:16:50 -0500

    commit tree e1332b18472991e29b5415530da682f7a8870c9c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(driverNames, string1) {
+    let match = driverNames.filter(match => match.toLowerCase() === string1.toLowerCase());
+    return match;
+}
+
+function fuzzyMatch(driverNames, string1) {
+    let startsWithLetters = driverNames.filter((driver) => driver.startsWith(string1));
+    return startsWithLetters;
+}
+
+function matchName(driverNames, string1) {
+    let matchEverything = driverNames.filter(match => match.name === string1);
+    return matchEverything;
+}
